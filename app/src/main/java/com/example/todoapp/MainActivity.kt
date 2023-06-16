3ceb81c2299b19bf3219a646cbdae675bfe6fbf2
red19958 <sadistick234@gmail.com>
2023-06-16 17:45:03 +0300

    HW1 commit
    Thanks for watching

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.todoapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.todoapp.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var bundle: Bundle? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun getBundle(): Bundle? {
+        return bundle
+    }
+
+    fun setBundle(bundle: Bundle) {
+        this.bundle = bundle
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/MyDiffUtilCallback.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp
+
+import androidx.recyclerview.widget.DiffUtil
+
+
+class MyDiffUtilCallback(
+    private val oldList: List<TodoItem>,
+    private val newList: List<TodoItem>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize() = oldList.size
+
+    override fun getNewListSize() = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItemId = oldList[oldItemPosition].id
+        val newItemId = newList[newItemPosition].id
+        return oldItemId === newItemId
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem == newItem
+    }
+}
